1d380ad1e484da90334b7691b6a3636e6d0a0d4f
Huy Nguyen <botter1211@gmail.com>
2023-12-30 17:06:11 +0700

    huy commit

--- src/formatTime.js
+++ src/formatTime.js
@@ -1,8 +1,11 @@
 export const formatTime = (time) => {
-  const getSeconds = "Your code here";
-  const minutes = "Your code here";
-  const getMinutes = "Your code here";
-  const getHours = "Your code here";
+  let x = time % 360;
+  let y = x % 60;
+  const getSeconds = y < 10 ? `0${y}` : y;
+  const minutes = (x - y) / 60;
+  const getMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const hours = (time - x) / 360;
+  const getHours = hours < 10 ? `0${hours}` : hours;
 
   return `${getHours} : ${getMinutes} : ${getSeconds}`;
 };
